31a812195d10656b8a11918d354c188bd1cb2139
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-08-12 20:47:31 +0900

    edit

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -14,10 +14,12 @@ int main() {
 
 	for (int i = 0; i < N; i++) {
 		cin >> temp;
+		//크기같은 예외 주의
 		if (first.length()+last.length() > temp.length()) {
 			cout << "NE\n";
 		}
 		else {
+			//아이디어.
 			if (first == temp.substr(0, first.length()) && last == temp.substr(temp.length() - last.size())) cout << "DA\n";
 			else cout << "NE\n";
 		}
