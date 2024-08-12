85ffd46abbf8fe6336e09e934bf1090ec20099ee
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-08-12 19:37:38 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N;
+string first, last ,str, temp;
+
+int main() {
+	cin >> N;
+	cin >> str;
+	first = str.substr(0, str.find("*"));
+	last = str.substr(str.find("*") + 1);
+
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		if (first.length()+last.length() > temp.length()) {
+			cout << "NE\n";
+		}
+		else {
+			if (first == temp.substr(0, first.length()) && last == temp.substr(temp.length() - last.size())) cout << "DA\n";
+			else cout << "NE\n";
+		}
+
+	}
+}
