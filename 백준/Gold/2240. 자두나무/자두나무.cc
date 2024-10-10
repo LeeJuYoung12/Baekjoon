d703828b2b93550b81aa2c9af3871207550c3228
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-10-10 14:24:11 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <stack>
+#include <queue>
 #include <cstring>
-
+#define INF 1e9
 using namespace std;
 
+int T, W, arr[1004], dp[1004][2][34];
 
-int T, W, ret, arr[1004], dp[1004][2][34];
+int go(int t, int tree, int w){
+    
+    if (w < 0) return -1e9;
+    if (t > T) return 0;
 
-int go(int idx, int tree, int cnt) {
-    if (cnt < 0) return -1e9;
-    if (idx == T) {
-        return 0;
-    }
+    int& ret = dp[t][tree][w];
+    if (ret != 0) return ret;
 
-    int& ret = dp[idx][tree][cnt];
-    if (~ret) return ret;
-     
-    return ret = max(go(idx + 1, tree ^ 1, cnt - 1), go(idx + 1, tree, cnt)) + (tree == arr[idx] - 1);
+    return ret = max(go(t + 1, tree, w), go(t + 1, (tree + 1) % 2, w - 1)) + (tree == arr[t] - 1);
 }
 
 int main() {
@@ -26,9 +24,8 @@ int main() {
     cin.tie(0); cout.tie(0);
 
     cin >> T >> W;
-    memset(dp, -1, sizeof(dp));
-    for (int i = 0; i < T; i++) {
-        cin >> arr[i];
-    }
-    cout << max(go(0, 1, W - 1), go(0, 0, W)) << "\n";
-}   
+    
+    for (int i = 1; i <= T; i++) cin >> arr[i];
+
+    cout << max(go(1, 0, W), go(1, 1, W - 1))<<"\n";
+}
