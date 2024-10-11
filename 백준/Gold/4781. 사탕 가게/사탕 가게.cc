7fbc7c5f6e5627a7f01b2d217e2c498a53b6bf40
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-10-11 16:37:22 +0900

    [Gold IV] Title: 사탕 가게, Time: 180 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define INF 1e9
+using namespace std;
+
+int N, M1, M2, C, dp[100004];
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    while (1) {
+        scanf("%d %d.%d", &N, &M1, &M2);
+        
+        if (N == 0) break;
+        int cost = M1 * 100 + M2;
+        fill(dp, dp + 100004, 0);
+
+        for (int i = 0; i < N; i++) {
+            scanf("%d %d.%d", &C, &M1, &M2);
+            int p = M1 * 100 + M2;
+            for (int j = p; j <= cost; j++) {
+                dp[j] = max(dp[j], dp[j - p] + C);
+            }
+        }
+
+        cout << dp[cost]<<"\n";
+    }
+}
