df685132900e29f15e7eb9a0acfba2058ce44e31
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-10-13 00:55:32 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,32 +1,30 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int dp[21][101];
-int main()
-{
-    int N;
-    int L[21], J[21], M = -1;
-    
-    cin>>N;
-    for(int i = 1;i<=N;i++){
-        cin>>L[i];
+int N, L[24], J[24], ret, dp[104];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> L[i];
     }
-        for(int i = 1;i<=N;i++){
-        cin>>J[i];
+
+    for (int i = 0; i < N; i++) {
+        cin >> J[i];
     }
-    
-    for(int i =1;i<=N;i++){
-        for(int j=100;j>=0;j--){
-            if(j>L[i]){
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-L[i]]+J[i]);
-            }
-            else{
-                dp[i][j] = dp[i-1][j];
-            }
-            if(dp[i][j]>=M) M = dp[i][j];
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 100; j > L[i]; j--) {
+            dp[j] = max(dp[j], dp[j - L[i]] + J[i]);
         }
     }
-    cout<<M;
+ 
+
+    // 결과 출력
+    cout << dp[100] << "\n";
 }
