0c7629f14671341a545e272778cc626c1311091d
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-11-13 15:11:04 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,30 +1,26 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <cstring>
+#define INF 987654321
 using namespace std;
 
-int N, L[24], J[24], ret, dp[104];
+int N, L[24], J[24], dp[104];
+
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
+
     cin >> N;
-
-    for (int i = 0; i < N; i++) {
-        cin >> L[i];
-    }
-
-    for (int i = 0; i < N; i++) {
-        cin >> J[i];
-    }
+    for (int i = 0; i < N; i++) cin >> L[i];
+    for (int i = 0; i < N; i++) cin >> J[i];
 
     for (int i = 0; i < N; i++) {
         for (int j = 100; j > L[i]; j--) {
             dp[j] = max(dp[j], dp[j - L[i]] + J[i]);
         }
     }
- 
-
-    // 결과 출력
     cout << dp[100] << "\n";
 }
