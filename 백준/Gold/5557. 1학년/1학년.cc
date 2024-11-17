77a725c6d24dd9de48b2ad5ad8a2edfb28e0c2b9
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-11-17 13:44:35 +0900

    [Gold V] Title: 1학년, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.cc
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -1,59 +1,31 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <cstring>
+#define INF 987654321
 using namespace std;
 
-long long N, A[104], dp[104][24], ret;
+long long N, A[104], dp[104][24];
 
-bool check(long long a, long long b, long long oper) {
-    long long temp;
-    if (oper == 0) {  // 덧셈
-        temp = a + b;
+long long go(int idx, int sum) {
+    if (sum < 0 || sum>20) return 0;
+    if (idx == N - 1) {
+        return (sum == A[idx]) ? 1 : 0;
     }
-    else if (oper == 1) {  // 뺄셈
-        temp = a - b;
-    }
-    else {
-        return false;
-    }
-    return temp >= 0 && temp <= 20;  // 유효 범위 확인
+    long long& ret = dp[idx][sum];
+    if (ret != -1) return ret;
+    ret = 0;
+    return ret = go(idx + 1, sum + A[idx]) + go(idx + 1, sum - A[idx]);
 }
-
-void go(int s, int idx) {
-    if (idx == N - 1) {  // 마지막까지 탐색
-        return;
-    }
-    // 0: 덧셈
-    if (check(s, A[idx + 1], 0)) {
-        long long temp = s + A[idx + 1];
-        dp[idx + 1][temp] += dp[idx][s];
-    }
-    // 1: 뺄셈
-    if (check(s, A[idx + 1], 1)) {
-        long long temp = s - A[idx + 1];
-        dp[idx + 1][temp] += dp[idx][s];
-    }
-}
+//8 3 2 4 (4
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    memset(dp, -1, sizeof(dp));
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> A[i];
     }
-
-    dp[0][A[0]] = 1;  // 시작 상태 초기화
-
-    for (int i = 0; i < N - 1; i++) {
-        for (int j = 0; j <= 20; j++) {
-            if (dp[i][j] > 0) {
-                go(j, i);  // 현재 숫자와 인덱스에서 가능한 연산 호출
-            }
-        }
-    }
-
-    cout << dp[N - 2][A[N - 1]] << "\n";  // 마지막 결과 출력
+    cout << go(1, A[0]) << "\n";
 }
