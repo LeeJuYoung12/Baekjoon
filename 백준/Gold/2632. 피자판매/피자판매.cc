7f4f0a30a6c7434986be6a38a406650cb3f76472
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-11-19 15:08:07 +0900

    [Gold II] Title: 피자판매, Time: 504 ms, Memory: 66488 KB -BaekjoonHub

---
+++ 백준/Gold/2632. 피자판매/피자판매.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#define INF 987654321
+using namespace std;
+
+int N, M, K, A[2004], B[2004], psumA[4004], psumB[4004], ret;
+
+map<int, int> make(int n, int psum[]) {
+    map<int, int> mp;
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n + i; j++) {
+            int sum = psum[j] - psum[j - i];
+            mp[sum]++;
+            if (i == n) break;
+        }
+    }
+    return mp;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    cin >> K;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+        psumA[i] = psumA[i - 1] + A[i];
+    }
+    for (int i = N + 1; i <= 2 * N; i++) {
+        psumA[i] = psumA[i - 1] + A[i - N];
+    }
+
+    for (int i = 1; i <= M; i++) {
+        cin >> B[i];
+        psumB[i] = psumB[i - 1] + B[i];
+    }
+    for (int i = M + 1; i <= 2 * M; i++) {
+        psumB[i] = psumB[i - 1] + B[i - M];
+    }
+
+    map<int, int> aCnt = make(N, psumA);
+    map<int, int> bCnt = make(M, psumB);
+
+    ret = aCnt[K] + bCnt[K];
+    for (int i = 1; i < K; i++) {
+        ret += (aCnt[i] * bCnt[K - i]);
+    }
+    cout << ret << "\n";
+}
