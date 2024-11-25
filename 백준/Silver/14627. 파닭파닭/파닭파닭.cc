fbaa0b90d736ba719e0e757e39b6e79aa2161087
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-11-25 14:55:23 +0900

    [Silver II] Title: 파닭파닭, Time: 360 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/14627. 파닭파닭/파닭파닭.cc
+++ 백준/Silver/14627. 파닭파닭/파닭파닭.cc
@@ -1,59 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define INF 1e9
-
 using namespace std;
 
-long long S, C, L, r, ret , sum;
+long long S, C, mid, l = 1, r, totalSum = 0, result = 0;
 vector<long long> v;
 
-bool check(long long mid) {
-    long long cnt = 0, _sum = 0, temp, i;
-    bool flag = false;
-    for (i = 0; i < S; i++) {
-        cnt += v[i] / mid;
-        _sum += v[i] % mid;
-        if (cnt >= C) {
-            flag = true;
-            break;
-        } 
+bool check(long long m) {
+    long long cnt = 0;
+    for (long long length : v) {
+        cnt += length / m; // 막대길이로 잘라낼 수 있는 막대 수
     }
-    if (flag) {       
-        return true;
-    }
-    else {
-        return false;
-    }
+    return cnt >= C;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    
+
     cin >> S >> C;
-    
     for (int i = 0; i < S; i++) {
+        long long L;
         cin >> L;
-        sum += L;
         v.push_back(L);
+        totalSum += L; // 전체 막대 길이의 합
+        r = max(r, L); // 최대 막대 길이
     }
-    
-    sort(v.begin(), v.end());
-    long long l = 1, r = v[S - 1], mid;
 
     while (l <= r) {
         mid = (l + r) / 2;
-      
-        if (check(mid)) {//가능. 작아지는 경우
-            l = mid + 1; 
-            ret = mid;
-        }
-        else {
+        if (check(mid)) {
+            result = mid; // 현재 최대 길이를 저장
+            l = mid + 1;
+        } else {
             r = mid - 1;
         }
-        
     }
 
-    cout << sum-ret*C << "\n";
-}   
+    // 결과 계산
+    long long usedLength = result * C; // 사용된 총 길이
+    long long leftover = totalSum - usedLength; // 남는 총 길이
+
+    cout << leftover << "\n";
+}
