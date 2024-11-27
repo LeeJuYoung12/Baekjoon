c8172266c8538e7f490d8ce0ff5d63b0afea03e6
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-11-27 15:02:51 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
@@ -1,77 +1,89 @@
 #include <iostream>
-#include <algorithm>
-#include <deque>
-#include <cstring>
 #include <vector>
-#define INF 987654321
-
+#include <algorithm>
+#include <cstring>
+#include <map>
+#include <queue>
+#define INF 60000000004
 using namespace std;
 
-int T, K, a, b, isRotate[1004], ret;
-string arr[1004];
+int T, temp, K, a, b, ret;
+vector<int> v[1004];
+string str;
 
-void rotateFunc() {
-	for (int i = 0; i < T; i++) {
-		if (isRotate[i] == 1) { //시계
-			char temp = arr[i][arr[i].size() - 1];
-			for (int j = arr[i].size()-1; j > 0; j--) {
-				arr[i][j] = arr[i][j - 1];
-			}
-			arr[i][0] = temp;
-		}
-		else if (isRotate[i] == -1) {
-			char temp = arr[i][0];
-			for (int j = 0; j < arr[i].size() - 1; j++) {
-				arr[i][j] = arr[i][j + 1];
-			}
-			arr[i][arr[i].size() - 1] = temp;
-		}
-	}
+void rotateFunc(int num, int dir) {
+    int now1, now2, tempDir;
+
+    now1 = v[num][2];
+    now2 = v[num][6];
+
+    if (dir == 1) {
+        rotate(v[num].rbegin(), v[num].rbegin() + 1, v[num].rend());
+    }
+    else {
+        rotate(v[num].begin(), v[num].begin() + 1, v[num].end());
+    }
+    
+    tempDir = dir;
+    //right
+    for (int i = num + 1; i < T; i++) {
+        if (now1 == v[i][6]) {
+            break;
+        }
+        else {
+            now1 = v[i][2];
+            if (tempDir == 1) {
+                rotate(v[i].begin(), v[i].begin() + 1, v[i].end());
+            }
+            else {
+                rotate(v[i].rbegin(), v[i].rbegin() + 1, v[i].rend());
+            }
+            tempDir *= -1;
+        }
+    }
+
+    tempDir = dir;
+    //left
+    for (int i = num - 1; i >= 0; i--) {
+        if (now2 == v[i][2]) {
+            break;
+        }
+        else {
+            now2 = v[i][6];
+            if (tempDir == 1) {
+                rotate(v[i].begin(), v[i].begin() + 1, v[i].end());
+            }
+            else {
+                rotate(v[i].rbegin(), v[i].rbegin() + 1, v[i].rend());
+            }
+            tempDir *= -1;
+        }
+    }
 }
 
-void go(int idx, int dir) {
-	//1 시계방향 1 S극
-	fill(&isRotate[0], &isRotate[0] + 1004, 0);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        cin >> str;
+        for (int j = 0; j < str.length(); j++) {
+            v[i].push_back(str[j] - '0');
+        }
+    }
 
-	isRotate[idx] = dir;
+    cin >> K;
+    for (int i = 0; i < K; i++) {
+        cin >> a >> b;
+        a--;// 1 시계 -1 반시계
+        rotateFunc(a, b);
+    }
 
-	for (int i = idx; i < T - 1; i++) {
-		if (arr[i][2] != arr[i + 1][6]) {
-			isRotate[i + 1] = -isRotate[i];
-		}
-	}
-
-	for (int i = idx; i >= 1; i--) {
-		if (arr[i][6] != arr[i - 1][2]) {
-			isRotate[i - 1] = -isRotate[i];
-		}
-	}
-	rotateFunc();
-
-	return;
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	
-	cin >> T;
-	for (int i = 0; i < T; i++) {
-		cin >> arr[i];
-	}
-
-	cin >> K;
-	for (int i = 0; i < K; i++) {
-		cin >> a >> b;
-		a--;
-		go(a, b);
-	}
-
-	for (int i = 0; i < T; i++) {
-		if (arr[i][0] == '1') {
-			ret++;
-		}
-	}
-	cout << ret;
+    for (int i = 0; i < T; i++) {
+        if (v[i][0] == 1) {
+            ret++;
+        }
+    }
+    cout << ret << "\n";
 }
