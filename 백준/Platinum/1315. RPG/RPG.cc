1d52f64e86ed5db9a8f28b215c346212b8f3e680
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-11-30 14:01:50 +0900

    [Platinum III] Title: RPG, Time: 380 ms, Memory: 5960 KB -BaekjoonHub

--- 백준/Platinum/1315. RPG/RPG.cc
+++ 백준/Platinum/1315. RPG/RPG.cc
@@ -2,59 +2,54 @@
 #include <vector>
 #include <algorithm>
 #include <cstring>
+#include <map>
+#include <cmath>
+#define INF 60000000004
 using namespace std;
 
-int N, dp[1004][1004], visited[104];
-
-struct que {
-    int s, i, p;
-    que(int x, int y, int c) :s(x), i(y), p(c) {}
-
-};
-vector<que> a;
+int N, Str[54], Int[54], Pnt[54], nowStr = 1, nowInt = 1, visited[54], dp[1004][1004];
 
 int go(int STR, int INT) {
-     
-    int &ret = dp[STR][INT];
+    
+    int& ret = dp[STR][INT];
     if (ret != -1) return ret;
 
-    int pnt = 0;
     ret = 0;
+    int pnt = 0;
     vector<int> v;
     for (int i = 0; i < N; i++) {
-        if (a[i].s <= STR || a[i].i <= INT) {
+        if (STR >= Str[i] || INT >= Int[i]) {
             ret++;
             if (!visited[i]) {
-                visited[i] = true;
-                pnt += a[i].p;
+                visited[i] = 1;
+                pnt += Pnt[i];
                 v.push_back(i);
             }
         }
     }
 
     for (int i = 0; i <= pnt; i++) {
-        int nextSTR = min(1000, STR + i);
-        int nextINT = min(1000, INT + pnt - i);
-        ret = max(ret, go(nextSTR, nextINT));
+        int nextStr = min(1000, STR + i);
+        int nextInt = min(1000, INT + pnt - i);
+        ret = max(ret, go(nextStr, nextInt));
     }
 
-    for (int q : v) {
-        visited[q] = false;
+    for (int here : v) {
+        visited[here] = 0;
     }
+
     return ret;
 }
-
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    
+    memset(dp, -1, sizeof(dp));
+
     cin >> N;
     for (int i = 0; i < N; i++) {
-        int STR, INT, PNT;
-        cin >> STR >> INT >> PNT;
-        a.push_back(que(STR, INT, PNT));
+        cin >> Str[i] >> Int[i] >> Pnt[i];
     }
-    memset(dp, -1, sizeof(dp));
+
     cout << go(1, 1) << "\n";
 }
