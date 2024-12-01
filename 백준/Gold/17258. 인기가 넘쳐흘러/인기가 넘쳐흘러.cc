2732b86f9743bbbe8e3b8329edc30e9fc9e33bd9
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-12-01 14:08:24 +0900

    [Gold I] Title: 인기가 넘쳐흘러, Time: 0 ms, Memory: 2384 KB -BaekjoonHub

--- 백준/Gold/17258. 인기가 넘쳐흘러/인기가 넘쳐흘러.cc
+++ 백준/Gold/17258. 인기가 넘쳐흘러/인기가 넘쳐흘러.cc
@@ -12,19 +12,19 @@ int go(int idx, int num, int prev) {
     if (dp[idx][num]) return dp[idx][num];
 
     int cost = max(0, T - v[idx].second);
-    int real_cost = (prev >= cost) ? 0 : cost;
+    int real_cost = (prev >= cost) ? 0 : cost - prev;  
 
     int& ret = dp[idx][num];
     if (num - real_cost >= 0) {
         return ret = max(go(idx + 1, num - real_cost, cost) + v[idx].first, go(idx + 1, num, 0));
     }
-    else return ret = go(idx + 1, num, 0);  
+    else return ret = go(idx + 1, num, 0);
 }
 // 0 0 1 1 2 2 3 2 2 1 1
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    
+
     cin >> N >> M >> K >> T;
     for (int i = 0; i < M; i++) {
         cin >> a >> b;
@@ -32,7 +32,7 @@ int main() {
             cnt[j] = min(T, ++cnt[j]);
         }
     }
-    
+
     int temp = cnt[1];
     int _count = 1;
     for (int i = 2; i <= N; i++) {
