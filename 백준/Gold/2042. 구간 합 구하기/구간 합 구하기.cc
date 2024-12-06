2c47a13f6b1dd14d736830c5215bf6dc8d434236
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-12-06 14:18:08 +0900

    [Gold I] Title: 구간 합 구하기, Time: 192 ms, Memory: 17644 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -2,54 +2,45 @@
 #include <vector>
 #include <algorithm>
 #include <cstring>
-#define size 1000001  // N의 최대 크기보다 1 크게 설정
+#include <map>
+#define INF 987654321
 using namespace std;
 
-long long N, M, K, arr[size], tree[size], a, b, c;
-
-void update(int idx, long long val) {
-    while (idx <= N) {  // idx가 N 이하일 때까지만 업데이트
-        tree[idx] += val;
-        idx += (idx & -idx);  // 다음 인덱스 이동
-    }
-}
-
-void make_Tree() {
-    for (int i = 1; i <= N; i++) {
-        update(i, arr[i]);
-    }
-}
+long long N, M, K, A[1000004], a, b, c, tree[1000004];
 
 long long sum(int idx) {
     long long ret = 0;
     while (idx > 0) {
         ret += tree[idx];
-        idx -= (idx & -idx);  // 이전 인덱스로 이동
+        idx -= (idx & -idx);
     }
     return ret;
+}
+
+void update(int idx, long long val) {
+    while (idx <= N) {
+        tree[idx] += val;
+        idx += (idx & -idx);
+    }
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-
+    
     cin >> N >> M >> K;
     for (int i = 1; i <= N; i++) {
-        cin >> arr[i];
+        cin >> A[i];
+        update(i, A[i]);
     }
 
-    make_Tree();  // 트리 생성
-
-    for (int i = 1; i <= M + K; i++) {
+    for (int i = 0; i < M + K; i++) {
         cin >> a >> b >> c;
         if (a == 1) {
-            // b번째 값을 c로 바꿈
-            long long diff = c - arr[b];
-            arr[b] = c;
-            update(b, diff);
-        }
+            update(b, c - A[b]);
+            A[b] = c;
+        }            
         else {
-            // b부터 c까지 합을 구함
             cout << sum(c) - sum(b - 1) << "\n";
         }
     }
