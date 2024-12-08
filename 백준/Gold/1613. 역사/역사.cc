27e49a34135a4f93afbf55a21e61154f1537b365
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-12-08 14:01:44 +0900

    [Gold III] Title: 역사, Time: 96 ms, Memory: 2660 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.cc
+++ 백준/Gold/1613. 역사/역사.cc
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <cmath>
+#include <algorithm>
+#include <cstring>
+#include <map>
 #define INF 987654321
+#define y1 AAA
 using namespace std;
 
-int N, K, S, A[404][404], a, b;
-
+int N, K, a, b, dist[404][404], s;
+  
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
@@ -14,22 +16,22 @@ int main() {
     cin >> N >> K;
     for (int i = 0; i < K; i++) {
         cin >> a >> b;
-        A[a][b] = -1;
-        A[b][a] = 1;
+        dist[a][b] = -1;
+        dist[b][a] = 1;
     }
-    
+
     for (int k = 1; k <= N; k++) {
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (A[i][k] == 1 && A[k][j] == 1) A[i][j] = 1;
-                else if(A[i][k] == -1 && A[k][j] == -1) A[i][j] = -1;
+                if (dist[i][k] == 1 && dist[k][j] == 1) dist[i][j] = 1;
+                if (dist[i][k] == -1 && dist[k][j] == -1) dist[i][j] = -1;
             }
         }
     }
-    cin >> S;
-    for (int i = 0; i < S; i++) {
+    
+    cin >> s;
+    for (int i = 0; i < s; i++) {
         cin >> a >> b;
-        cout << A[a][b] << "\n";
+        cout << dist[a][b] << '\n';
     }
-
 }
