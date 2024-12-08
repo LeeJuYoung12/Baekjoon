57e2caf74ce89695d159c3c3c64aa1ab6e4d1219
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-12-08 14:30:21 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 3236 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,42 +1,50 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-
+#include <algorithm>
+#include <cstring>
+#include <queue>
 #define INF 987654321
 using namespace std;
 
-long long N, M, ret, X, dist[1004][1004], a, b, t;
+int N, M, dist1[1004], dist2[1004], a, b, t, X, ret;
+vector<pair<int, int>> adj1[20004], adj2[20004];
+void dijkstra(int start, int *dist, vector<pair<int,int>> *adj) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    dist[start] = 0;
+    pq.push({ 0, start });
+    while (!pq.empty()) {
+        int here_cost = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        if (dist[u] != here_cost) continue;
+
+        for (auto there : adj[u]) {
+            int new_cost = here_cost + there.first;
+            if (new_cost < dist[there.second]) {
+                dist[there.second] = new_cost;
+                pq.push({ new_cost, there.second });
+            }
+        }
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
-    fill(&dist[0][0], &dist[0][0] + 1004 * 1004, INF);
-
+    fill(&dist1[0], &dist1[0] + 1004, INF);
+    fill(&dist2[0], &dist2[0] + 1004, INF);
     cin >> N >> M >> X;
-    X--;  // X에 대해서도 1을 빼서 0부터 시작하도록 설정
-
-    for (int i = 0; i < N; i++) dist[i][i] = 0; // 자기 자신으로의 거리 초기화
-
     for (int i = 0; i < M; i++) {
         cin >> a >> b >> t;
-        a--, b--;
-        dist[a][b] = t;
+        adj1[a].push_back({ t, b });
+        adj2[b].push_back({ t, a });
     }
-
-    // 플로이드 와샬 알고리즘
-    for (int k = 0; k < N; k++) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-            }
-        }
+    dijkstra(X, dist1, adj1);
+    dijkstra(X, dist2, adj2);
+    
+    for (int i = 1; i <= N; i++) {
+        ret = max(ret, dist1[i] + dist2[i]);
     }
-
-    for (int i = 0; i < N; i++) {
-        if (dist[i][X] != INF && dist[X][i] != INF) {  // 무한대 검사
-            ret = max(ret, dist[i][X] + dist[X][i]);
-        }
-    }
-
     cout << ret << "\n";
 }
