cf1d33765b180896a1b08a5d846a48b992a90334
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-12-20 15:11:06 +0900

    [Gold IV] Title: 알파벳, Time: 316 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,38 +1,40 @@
 #include <iostream>
-#include <map>
-
+#include <cstring>
+#include <algorithm>
+#define INF -987654321
 using namespace std;
 
-int R, C, visited[26], ret;
+int R, C, dp, visited[26], ret;
 char arr[24][24];
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-void DFS(int y, int x, int cnt){
+int dy[] = { -1, 0, 1, 0 };
+int dx[] = { 0, -1, 0, 1 };
+
+void go(int y, int x, int cnt) {
     ret = max(ret, cnt);
-    
-    for(int i = 0;i<4;i++){
+    for (int i = 0; i < 4; i++) {
         int ny = y + dy[i];
         int nx = x + dx[i];
-        if(ny<0||nx<0||ny>=R||nx>=C) continue;
+        if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
         int next = (int)(arr[ny][nx] - 'A');
-        if(visited[next] == 0){
+        if (!visited[next]) {
             visited[next] = 1;
-            DFS(ny, nx, cnt + 1);
+            go(ny, nx, cnt + 1);
             visited[next] = 0;
         }
-        
     }
 }
-int main()
-{
-    cin>>R>>C;
-    for(int i = 0;i<R;i++){
-        for(int j = 0;j<C;j++){
-            cin>>arr[i][j];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> R >> C;
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> arr[i][j];
         }
     }
-    visited[(int)arr[0][0]-'A'] = 1;
-    DFS(0, 0, 1);
-    cout<<ret<<'\n';
-    return 0;
+    visited[(int)arr[0][0] - 'A'] = 1;
+    go(0, 0, 1);
+    cout << ret << "\n";
 }
