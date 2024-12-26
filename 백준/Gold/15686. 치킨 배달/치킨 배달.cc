a12970478520eef7c2bfb87528bd21ba54e4e57e
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-12-26 15:31:25 +0900

    [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,46 +1,55 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
 #include <vector>
-#include <cstdlib>
-
+#include <queue>
+#define INF 987654321
 using namespace std;
 
-int N, M, arr[55][55], ret = 987654321;
-vector<pair<int, int>> house, chicken;
+int N, M, arr[54][54], ret = INF;//0빈칸 1집 2치킨집
+vector<pair<int, int>> chicken, house, temp;
 
-void go(int d, vector<int> v) {
-	if (v.size() == M) {
-		int s = 0;
-		for (int i = 0; i < house.size(); i++) {
-			int _min = 987654321;
-			for (int j:v) {
-				int dis = abs(house[i].first - chicken[j].first) + abs(house[i].second - chicken[j].second);
-				_min = min(_min, dis);			
-			}
-			s += _min;
-		}
-		ret = min(ret, s);
-		return;
-	}
-	for (int i = d + 1; i < chicken.size(); i++) {
-		v.push_back(i);
-		go(i, v);
-		v.pop_back();
-	}
+int checkFunc() {
+    int ret = 0;
+    for (int i = 0; i < house.size(); i++) {
+        int distance = INF;
+        for (auto there : temp) {
+            int tempDistance = abs(there.first - house[i].first) + abs(there.second - house[i].second);
+            distance = min(distance, tempDistance);
+        }
+        ret += distance;
+    }
+
+    return ret;
 }
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cin >> arr[i][j];
-			if (arr[i][j] == 1) house.push_back({ i,j });
-			else if(arr[i][j] == 2) chicken.push_back({ i,j });
-		}
-	}
 
-	vector<int> v;
-	go(-1 ,v);
-	cout << ret;
+void go(int idx, int cnt) {
+    if (cnt == M) {
+        ret = min(ret, checkFunc());
+        return;
+    }
+    if (idx == chicken.size()) return;
+
+    go(idx + 1, cnt);
+
+    temp.push_back(chicken[idx]);
+    go(idx + 1, cnt + 1);
+    temp.pop_back();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> arr[i][j];
+            if (arr[i][j] == 1) house.push_back({ i,j });
+            else if (arr[i][j] == 2) chicken.push_back({ i,j });
+        }
+    }
+
+    go(0, 0);
+    cout << ret << '\n';
 }
