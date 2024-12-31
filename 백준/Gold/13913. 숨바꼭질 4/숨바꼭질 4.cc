925f27dca1b2f4b016be733afe2faec1f9a64a2a
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2024-12-31 15:09:29 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 3700 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,59 +1,70 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <cstring>
 #include <algorithm>
-
-#define max_n 200004    
-#define prev aaa
+#include <queue>
 using namespace std;
 
-int N, K, visited[max_n], prev[max_n];
-vector<int> v;
-void BFS(int _num) {
+int N, K, visited[100004], prevIdx[100004] , ret, temp;
+
+bool checkBound(int a) {
+    return (a >= 0 && a <= 100000);
+}
+
+void BFS(int start) {
     queue<int> q;
-    q.push(_num);
-    visited[_num] = 1;
-    while (q.size()) {           
-        int pos = q.front();
+    q.push(start);
+    visited[start] = 1;
+    
+    while (q.size()){
+        int now = q.front();
         q.pop();
-        if (pos == K) {
-            return;
+        if (now == K) {
+            ret = visited[now];
+            break;
         }
-        for (int k : {pos + 1, pos - 1, 2 * pos}) {
-            if (k < 0 || k>=max_n) continue;
-            if (!visited[k]) {
-                visited[k] = visited[pos] + 1;
-                prev[k] = pos;                
-                q.push(k);
+
+        for (auto next : { now + 1, now - 1, 2 * now }) {
+            if (!checkBound(next)) continue;
+
+            if (!visited[next]) {
+                q.push(next);
+                visited[next] = visited[now] + 1;
+                prevIdx[next] = now;
             }
         }
-    }
-    return;
+    }    
 }
-int main()
-{
+
+int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(0);
+    memset(prevIdx, -1, sizeof(prevIdx));
 
     cin >> N >> K;
-    
     if (N == K) {
         cout << 0 << '\n';
         cout << N << '\n';
+
         return 0;
     }
+
+    BFS(N);
+    cout << ret - 1 << '\n';
     
-
-    BFS(N);    
-    for (int i = K; i != N; i = prev[i]) {
-        v.push_back(i);
+    vector<int> v;
+    temp = K;
+    while (1) {
+        if (prevIdx[temp] == -1) {
+            v.push_back(temp);
+            break;
+        }
+        else {
+            v.push_back(temp);
+            temp = prevIdx[temp];
+        }
     }
-    v.push_back(N);
     reverse(v.begin(), v.end());
-
-    cout << visited[K] - 1 << '\n';
-    for (int i : v) cout << i << ' ';
- 
-    return 0;
+    for (auto a : v) {
+        cout << a << ' ';
+    }
 }
