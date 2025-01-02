3ebaf6cb5ecebe8f498531ce1e4b7f81b5f44d48
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2025-01-02 16:29:22 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.cc
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -1,57 +1,54 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
-#include <string>
 using namespace std;
 
-int N, visited[10];
-char arr[10];
-string _Min, _Max;
-vector<string> ret;
+string maxRet = "", minRet = "9999999999"; // 최대, 최소값 저장
+bool visited[10];
+char arr[14];
+int k; // 전역 변수로 k 선언
 
-bool comp(string a, string b) {
-    if (a.size() == b.size()) {
-        for (int i = 0; i < a.size(); i++) {
-            if (a[i] != b[i]) return a[i] < b[i];
-        }
-    }
-
-    return a.size() < b.size();
+bool check(int a, int b, char c) {
+    if (c == '>') return a > b;
+    else return a < b;
 }
 
-bool check(char op, char x, char y) {
-    if (x < y && op == '<') return true;
-    if (x > y && op == '>') return true;
-    return false;
-}
-
-void go(int idx, string temp) {
-    if (idx == N+1) {
-        ret.push_back(temp);
+void go(int idx, string str) {
+    // 숫자를 모두 만들었으면 결과 계산
+    if (idx == k + 1) {
+        if (str > maxRet) maxRet = str;
+        if (str < minRet) minRet = str;
         return;
     }
 
     for (int i = 0; i <= 9; i++) {
         if (visited[i]) continue;
-        if (idx == 0 || check(arr[idx - 1], temp[idx - 1], i + '0')) {
-            visited[i] = 1;
-            go(idx + 1, temp + to_string(i));
-            visited[i] = 0;
+
+        // 부등호 조건 확인
+        if (idx == 0 || check(str.back() - '0', i, arr[idx - 1])) {
+            visited[i] = true;
+            go(idx + 1, str + to_string(i));
+            visited[i] = false;
         }
     }
 }
 
-int main()
-{
-    cin >> N;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-    for (int i = 0; i < N; i++) {
+    cin >> k; // 입력받은 k 값 저장
+    for (int i = 0; i < k; i++) {
         cin >> arr[i];
     }
 
+    // 백트래킹 시작
     go(0, "");
 
-    sort(ret.begin(), ret.end());
-    cout << ret[ret.size() - 1] << '\n';
-    cout << ret[0] << '\n';
+    // 결과 출력
+    cout << maxRet << '\n';
+    cout << minRet << '\n';
+
+    return 0;
 }
