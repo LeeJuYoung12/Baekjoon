417f3f6dfa4dbadaf936e7d3b03656f3dc7fad00
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2025-01-10 15:13:30 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,23 +1,28 @@
 #include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
 #include <algorithm>
+#include <queue>
+#define INF 987654321
 using namespace std;
-int main()
-{
-    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
-    int N, T, ret, five = 0, two = 0;
-    cin>>T;
-    for(int i = 0; i<T;i++){
-        cin>>N;
-        two = 0; five = 0;
-        for(int j = 2;j<=N;j*=2){
-            two += N/j;
+
+int T, N;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> T;
+    while (T--) {
+        cin >> N;
+        int ret1 = 0, ret2 = 0;
+        for (int j = 2; j <= N; j*=2) {
+            ret1 += N / j;
         }
-        for(int j = 5;j<=N;j*=5){
-            five += N/j;
+        for (int j = 5; j <= N; j *= 5) {
+            ret2 += N / j;
         }
-        ret = min(five, two);
-        cout<<ret<<'\n';
-        
+        cout << min(ret1, ret2) << '\n';
     }
-    
 }
