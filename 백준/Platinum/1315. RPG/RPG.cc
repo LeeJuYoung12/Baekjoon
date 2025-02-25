42bd51223451d71957d14c9ca8d70ade58ef52af
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2025-02-25 17:30:31 +0900

    [Platinum III] Title: RPG, Time: 364 ms, Memory: 5960 KB -BaekjoonHub

--- 백준/Platinum/1315. RPG/RPG.cc
+++ 백준/Platinum/1315. RPG/RPG.cc
@@ -1,55 +1,53 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <cstring>
-#include <map>
-#include <cmath>
-#define INF 60000000004
+#include <vector>
+#include <queue>
+#include <algorithm>
+#define INF 987654321
 using namespace std;
 
-int N, Str[54], Int[54], Pnt[54], nowStr = 1, nowInt = 1, visited[54], dp[1004][1004];
+int N, STR[54], INT[54], PNT[54], visited[54], dp[1004][1004];
 
-int go(int STR, int INT) {
-    
-    int& ret = dp[STR][INT];
+int go(int s, int i) {
+    int& ret = dp[s][i];
     if (ret != -1) return ret;
 
     ret = 0;
     int pnt = 0;
     vector<int> v;
-    for (int i = 0; i < N; i++) {
-        if (STR >= Str[i] || INT >= Int[i]) {
+    for (int k = 0; k < N; k++) {
+        if (s >= STR[k] || i >= INT[k]) {
             ret++;
-            if (!visited[i]) {
-                visited[i] = 1;
-                pnt += Pnt[i];
-                v.push_back(i);
+            if (!visited[k]) {
+                visited[k] = 1;
+                pnt += PNT[k];
+                v.push_back(k);
             }
         }
     }
 
-    for (int i = 0; i <= pnt; i++) {
-        int nextStr = min(1000, STR + i);
-        int nextInt = min(1000, INT + pnt - i);
-        ret = max(ret, go(nextStr, nextInt));
+    for (int k = 0; k <= pnt; k++) {
+        int nextSTR = min(1000, s + k);
+        int nextINT = min(1000, i + pnt - k);
+        ret = max(ret, go(nextSTR, nextINT));
     }
 
-    for (int here : v) {
-        visited[here] = 0;
+    for (auto it : v) {
+        visited[it] = 0;
     }
-
     return ret;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0); 
     memset(dp, -1, sizeof(dp));
 
     cin >> N;
     for (int i = 0; i < N; i++) {
-        cin >> Str[i] >> Int[i] >> Pnt[i];
+        cin >> STR[i] >> INT[i] >> PNT[i];
     }
 
-    cout << go(1, 1) << "\n";
+    cout << go(1, 1) << '\n';
 }
+    
