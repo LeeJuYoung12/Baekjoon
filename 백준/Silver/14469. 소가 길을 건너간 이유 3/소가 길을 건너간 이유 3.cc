308821855ccf2c7c8a9a790a8dbb9771b97f7c96
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2025-03-29 00:41:07 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -1,40 +1,24 @@
 #include <iostream>
 #include <algorithm>
-#include <queue>
 #include <vector>
-
+#define INF 1e9
 using namespace std;
 
-int N, a, b, ret;
-vector <pair<int, int>> v;
-priority_queue<int, vector<int>, greater<int>> pq;
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-	
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> a >> b;
-		v.push_back({a, b});
-	}
-	sort(v.begin(), v.end());
+int N, a, b, now;
+vector<pair<int, int>> v;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a >> b;
+        v.push_back({ a, b });
+    }
+    sort(v.begin(), v.end());
+    for (auto it : v) {
+        if (now < it.first) now = it.first;
+        now += it.second;
+    }
 
-	int start = v[0].first;
-
-	for (int i = 0; i < N; i++) {
-		if (start > v[i].first) {
-			start = start;
-		}
-		else {
-			start = v[i].first;
-		}
-
-		start += v[i].second;
-	 }
-	
-
-	cout << start << '\n';
-	return 0;
+    cout << now;
 }
-
