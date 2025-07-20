724b8eb957f7ab3b63bd8d53f19cf4e7997d9483
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2025-07-20 19:27:27 +0900

    [Gold V] Title: 보물섬, Time: 116 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -1,59 +1,53 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <queue>
-
+#include <cstring>
+#include <vector>
+#define INF 1e9
 using namespace std;
 
-int N, M, visited[55][55], ret;
-char arr[55][55];
+int N, M, visited[54][54], ret = -INF;
+char arr[54][54];
+string str;
+int dy[] = { -1,0,1,0 };
+int dx[] = { 0,-1,0,1 };
 
-int dy[] = { -1, 0, 1, 0 };
-int dx[] = { 0, -1, 0, 1 };
-
-void BFS(int i, int j) {
-
-	queue < pair<int, int>> q;
-	q.push({ i,j });
-	visited[i][j] = 1;
+void BFS(int y,int x) {
+	queue<pair<int, int>> q;
+	q.push({ y, x });
+	visited[y][x] = 1;
 	while (q.size()) {
 		int y = q.front().first;
 		int x = q.front().second;
 		q.pop();
-
 		for (int i = 0; i < 4; i++) {
 			int ny = y + dy[i];
 			int nx = x + dx[i];
-			if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-			if (arr[ny][nx] == 'W' || visited[ny][nx]) continue;
+			if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+			if (visited[ny][nx] || arr[ny][nx] == 'W') continue;
 			visited[ny][nx] = visited[y][x] + 1;
-			q.push({ ny,nx });
-			ret = max(ret, visited[ny][nx]);
-
+			q.push({ ny, nx });
+			ret = max(ret, visited[ny][nx] - 1);
 		}
 	}
-	
 }
 
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
+
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
+		cin >> str;
 		for (int j = 0; j < M; j++) {
-			cin >> arr[i][j];
+			arr[i][j] = str[j];
 		}
 	}
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			if (arr[i][j] == 'L') {
-				fill(&visited[0][0], &visited[0][0] + 55 * 55, 0);
+				memset(visited, 0, sizeof(visited));
 				BFS(i, j);
 			}
 		}
 	}
-
-	cout << ret-1;
+	cout << ret << '\n';
 }
