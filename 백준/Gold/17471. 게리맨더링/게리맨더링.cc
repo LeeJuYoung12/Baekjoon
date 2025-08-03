370a2b1842a351d80e34df7c00d8c8a280f5c1e1
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2025-08-04 00:43:54 +0900

    [Gold III] Title: 게리맨더링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.cc
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.cc
@@ -1,92 +1,73 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
+#include <string>
+#include <cstring>
 #include <vector>
-#include <cmath>
-#define INF 987654321
+#include <map>
+#define INF 1e9
 using namespace std;
 
-int N, ret = INF, arr[14], num, visited[14], areaNum[14];
+int N, arr[14], areaNum[14], visited[14], m, temp, ret = INF;
 vector<int> adj[14];
 
-void DFS(int here, int area) {
-	visited[here] = 1;
-	for (int next : adj[here]) {
-		if (visited[next]) continue;
-		if (areaNum[next] != area) continue;
-		DFS(next, area);
+void dfs(int idx, int num) {
+	visited[idx] = 1;
+	for (int next : adj[idx]) {
+		if (visited[next] || areaNum[next] != num) continue;
+		dfs(next, num);
 	}
 }
 
-bool isConnected(int area) {
-	
-	int start = -1;
-	
-	for (int i = 0; i < N; i++) {
-		if (areaNum[i] == area) {
-			start = i;
-			break;
-		}
-	}
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0);
 
-	if (start == -1) return false;
-
-	DFS(start, area);
-
-	for(int i = 0;i<N;i++){
-		if (areaNum[i] == area && !visited[i]) return false;
-	}
-
-	return true;
-}
-
-
-int main()
-{
 	cin >> N;
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i <= N; i++) {
 		cin >> arr[i];
 	}
 
-	for (int i = 0; i < N; i++) {
-		cin >> num;
-		for (int j = 0; j < num; j++) {
-			
-			int temp;
+	for (int i = 1; i <= N; i++) {
+		cin >> m;
+		for (int j = 0; j < m; j++) {
 			cin >> temp;
-			temp--;
 			adj[i].push_back(temp);
-			adj[temp].push_back(i);
 		}
 	}
-	
-	for (int i = 0; i < (1 << N); i++) {
-		
-		bool flag = false;
-		int sumA = 0, sumB = 0;
 
-		fill(&visited[0], &visited[0] + 14, 0);
-		fill(&areaNum[0], &areaNum[0] + 14, 0);
+	for (int i = 0; i < (1 << N); i++) {
+		memset(areaNum, 0, sizeof(areaNum));
+		memset(visited, 0, sizeof(visited));
+
+		int cnt1 = 0, cnt2 = 0, sum1 = 0, sum2 = 0;
 
 		for (int j = 0; j < N; j++) {
 			if (i & (1 << j)) {
-				areaNum[j] = 1;//A area check
-				sumA += arr[j];
+				areaNum[j + 1] = 1;
+				sum1 += arr[j + 1];
 			}
 			else {
-				sumB += arr[j];
+				sum2 += arr[j + 1];
 			}
-		}		
+		}
 
-		if (isConnected(1) && isConnected(0)) {
-			ret = min(ret, abs(sumA - sumB));
+		
+		for (int j = 1; j <= N; j++) {
+			if (areaNum[j] == 1 && !visited[j]) {
+				dfs(j, 1);
+				cnt1++;
+			}
+			if (areaNum[j] == 0 && !visited[j]) {
+				dfs(j, 0);
+				cnt2++;
+			}
+		}
+
+		if (cnt1 == 1 && cnt2==1) {
+			ret = min(ret, abs(sum1 - sum2));
 		}
 	}
-	if (ret == INF) {
-		cout << -1 << '\n';
-	}
-	else {
-		cout << ret << '\n';
-	}
+	if (ret == INF) cout << -1 << '\n';
+	else cout << ret << "\n";
 }
  
