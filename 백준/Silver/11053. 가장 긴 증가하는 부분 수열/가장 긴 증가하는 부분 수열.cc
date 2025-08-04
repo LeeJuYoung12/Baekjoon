2af8af92fef8a03e6972529bc83c44acc4d5329c
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2025-08-04 22:33:32 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,29 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
-#include <stack>
-
+#include <string>
+#include <cstring>
+#include <vector>
+#include <map>
+#define INF 1e9
 using namespace std;
-
 
 int N, arr[1004], cnt[1004], ret = 1;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0), cin.tie(0);
+	fill(cnt, cnt + 1004, 1);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
 
-    cin >> N;
-    for (int i = 0; i < N; i++) cin >> arr[i];
-
-    fill(cnt, cnt + 1004, 1);
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < i; j++) {
-            if (arr[j] < arr[i] && cnt[i] < cnt[j] + 1) {
-                cnt[i] = cnt[j] + 1;
-                if (ret < cnt[i]) ret = cnt[i];
-            }
-        }
-    }
-
-    cout << ret;
-}   
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j] && cnt[i] < cnt[j] + 1) {
+				cnt[i] = cnt[j] + 1;
+				ret = max(ret, cnt[i]);
+			}
+		}
+	}
+	cout << ret << '\n';
+}
+ 
