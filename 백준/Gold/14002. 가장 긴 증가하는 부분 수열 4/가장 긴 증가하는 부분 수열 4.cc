6f101d800479a15b88e5fc18953b3c1c6e40e15e
LeeJuYoung12 <101955125+LeeJuYoung12@users.noreply.github.com>
2025-08-06 22:32:28 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,46 +1,44 @@
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
-#define MAX_N 1000004
-
+#include <string>
+#include <cstring>
+#include <vector>
+#include <map>
+#define INF 1e9
 using namespace std;
 
-int arr[MAX_N], cnt[MAX_N], N, prev_list[MAX_N], ret = 1, idx;
-vector<int> retV;
-
+int N, arr[1004], cnt[1004], ret = 1, prevIdx[1004], idx;
+vector<int> v;
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
-    }
+	ios_base::sync_with_stdio(0), cin.tie(0);
+	fill(cnt, cnt + 1004, 1);
+	fill(prevIdx, prevIdx + 1004, -1);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
 
-    fill(prev_list, prev_list + MAX_N, -1);
-    fill(cnt, cnt + MAX_N, 1);
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j] && cnt[j] + 1 > cnt[i]) {
+				cnt[i] = cnt[j] + 1;
+				prevIdx[i] = j;
+				if (ret < cnt[i]) {
+					idx = i;
+					ret = cnt[i];
+				}
+			}
+		}
+	}
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < i; j++) {
-            if (arr[i] > arr[j] && cnt[i] < cnt[j] + 1) {
-                cnt[i] = cnt[j] + 1;
-                prev_list[i] = j;
-                if (ret < cnt[i]) {
-                    ret = cnt[i];
-                    idx = i;
-                }
-            }
-        }
-    }
-    cout << ret<<"\n";
+	cout << ret << '\n';
+	while (idx >= 0) {
+		v.push_back(arr[idx]);
+		idx = prevIdx[idx];
+	}
+	for (int i = v.size() - 1; i >= 0; i--) {
+		cout << v[i]<<' ';
+	}
 
-    int i = idx;
-    for (; prev_list[i] != -1; i = prev_list[i]) {
-        retV.push_back(arr[i]);
-    }
-
-    retV.push_back(arr[i]);
-    for (int i = retV.size() - 1; i >= 0; i--) {
-        cout << retV[i]<<" ";
-    }
-}   
+}
